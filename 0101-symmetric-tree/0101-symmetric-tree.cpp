43534b9e82a4628f0a9ff711732dd915ed9d4a01
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2024-12-15 23:26:39 -0500

    Update 0101-symmetric-tree.cpp

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Solved Recursively...
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
