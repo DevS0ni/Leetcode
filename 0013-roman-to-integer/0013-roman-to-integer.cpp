de070e777739461e0bed7b0ade6ffbee72e14de2
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2024-12-20 23:36:20 -0500

    Time: 11 ms (31.81%), Space: 13.2 MB (41.83%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanMap = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        int result = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && romanMap[s[i]] < romanMap[s[i + 1]]) {
+                result -= romanMap[s[i]];
+            } else {
+                result += romanMap[s[i]];
+            }
+        }
+        
+        return result;   
+    }
+};
